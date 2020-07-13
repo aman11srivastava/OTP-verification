8015329e8c8c36db82d9457d5fbd4fb14c91567c
aman11srivastava <aman11srivastava@gmail.com>
2020-07-13 20:55:15 +0530

    Updates

---
+++ precache-manifest.b25f2d6a40f296327ccfc54ce6016351.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73e4bbc43d9efe6940c3ff486116b814",
+    "url": "/OTP-verification/index.html"
+  },
+  {
+    "revision": "bf1fc30e97538ba598d6",
+    "url": "/OTP-verification/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "687aadf8dab20b7ef8d0",
+    "url": "/OTP-verification/static/js/2.b1bc44fc.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/OTP-verification/static/js/2.b1bc44fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf1fc30e97538ba598d6",
+    "url": "/OTP-verification/static/js/main.7b8d6564.chunk.js"
+  },
+  {
+    "revision": "332b119e3a61aa6361f1",
+    "url": "/OTP-verification/static/js/runtime-main.5c0d6c7c.js"
+  }
+]);
